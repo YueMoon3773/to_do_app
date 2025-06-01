ece6bf00e35518cd75702b30bdf9e7be4411539a
YueMoon3773 <giabaonguynhas@gmail.com>
2025-06-01 21:20:42 +0700

    Add template for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
-    entry: './src/assets/js/main.js',
+    entry: './src/assets/js/index.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
