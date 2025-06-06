60fe59c3a8ecda0405bae533cf29b9d29a14f61e
YueMoon3773 <giabaonguynhas@gmail.com>
2025-06-06 23:52:59 +0700

    Update + fix animation for modal. Add to-do management list

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,1 +1,28 @@
+/*
+1. a to-do management
+    - each todo is an object. each has title, detail, dueDate, priority, status (done/not done), category
+2. a to do list management
+    - Create an empty arr and store each to do in
+    - update/get the to do
+3. a module to render to screen
+*/
+
 import '../css/styles.css';
+
+import toDoListManage from './toDo';
+
+const t = toDoListManage();
+console.log(t);
+
+const addEditModalDateInp = document.querySelector('#addEditModalDateInp');
+const addEditModalBtn = document.querySelector('.addEditModalBtn');
+
+const addEditItemModal = document.querySelector('.addEditItemModal');
+
+addEditItemModal.addEventListener('submit', (e) => {
+    e.preventDefault();
+});
+
+addEditModalBtn.addEventListener('click', () => {
+    console.log(addEditModalDateInp.value);
+});
